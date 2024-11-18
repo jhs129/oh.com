69e2ef3f56cfcf0c101c5cb74fff40127dda1b46
jhs129 <john.h.schneider@gmail.com>
2024-11-18 14:21:55 -0500

    initial commit

--- builder-registry.js
+++ builder-registry.js
@@ -3,3 +3,17 @@ import dynamic from "next/dynamic";
 
 builder.init(process.env.NEXT_PUBLIC_BUILDER_API_KEY);
 
+Builder.registerComponent(
+    dynamic(() => import("@/components/cta/tile-cta")),
+    {
+      name: "Tile CTA",
+      inputs: [
+        { name: 'headline', type: 'string', required: true },
+        { name: 'body', type: 'string', required: true },
+        { name: 'ctaText', type: 'string', required: true },
+        { name: 'ctaHref', type: 'string', required: true },
+        { name: 'theme', type: 'string', required: true, defaultValue: 'light', enum: ['light', 'dark'] },
+    ],
+    }
+  );
+
